5de08ca1d775bd2d92f9381dbd9d07e0d81fadde
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-11-03 16:12:26 +0530

    display the questions

--- quiz.js
+++ quiz.js
@@ -1,9 +1,21 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+const questionObj = {
+  category: "Food & Drink",
+  id: "qa-1",
+  correctAnswer: "Three",
+  options: ["Two", "Three ", "Four", "Five"],
+  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+};
+
+//destructurin the questionobj property
+
+({ category, id, correctAnswer, options, question } = questionObj);
+
+const questionElement = document.getElementById("question");
+questionElement.textContent = question;
+
+const optionsElement = document.getElementById("options");
+options.forEach((option) => {
+  const optionbutton = document.createElement("button");
+  optionbutton.textContent = option;
+  optionsElement.appendChild(optionbutton);
+});
